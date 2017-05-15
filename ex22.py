84a87ee94cdc8bf7aef8ca295df9875060ef79b0
Sanji Jain <sanjitjain2@Gmail.com>
2017-05-15 08:58:36 +0530

    Adding ex22.py

--- ex22.py
+++ ex22.py
@@ -1,28 +1,75 @@
 people = 20 
-cats = 30
-dogs = 15
+cars = 30
+buses = 15
 
-if people < cats:
-    print "too many cats"
+#if people < cats:
+#    print "too many cats"
     
-if people > cats:   
-    print "too many dogs"
+#if people > cats:   
+#    print "too many dogs"
     
-if people > dogs:
-    print "too many people"
-    
-if people < dogs:
-    print "too many dogs"
-    
-dogs += 5
+#if people > dogs:
+#    print "too many people"   
 
-if people >= dogs:
-    print "people are greater than or equal to dogs"
+#if people < dogs:
+#    print "too many dogs"
     
-if people <= dogs:
-    print "dogs are greater than or equal to people"
+#dogs += 5
+
+#if people >= dogs:
+#    print "people are greater than or equal to dogs"
+    
+#if people <= dogs:
+#    print "dogs are greater than or equal to people"
         
-if people == dogs:
-    print "people are drugs"
+#if people == dogs:
+#    print "people are drugs"
     
+ 
+if cars > people:
+	print "cars are more"
+    
+elif cars < people:
+    	print "people are more"
+    
+else:   
+	print "we cant decide"
+      
+if buses > cars:
+	print "buses are more"
+
+elif cars > buses:
+	print "cars are more"
+
+else:
+	print "we cant decide"
+
+if people > buses:
+	print "people are more"
+
+else:	
+	print "people are equal or less"	        
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     
